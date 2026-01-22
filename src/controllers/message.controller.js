3be44c9be980634505ba71ca33acecaf1a899d67
Izhan07 <izhanahmed987@gmail.com>
2026-01-22 22:00:14 +0500

    Implement message retrieval functionalities and update message routes

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/Apierror.js";
 import { User } from "../models/user.model.js";
 import { Message } from "../models/message.model.js";
-import { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -13,7 +13,6 @@ const sendMessage = asyncHandler( async (req, res) => {
     }
     const { text } = req.body;
     const mediaLocalPath = req.files;
-    console.log(mediaLocalPath)
     if(!text.trim() && (!mediaLocalPath || mediaLocalPath.length === 0)){
         throw new ApiError(400, "Content required")
     }
@@ -49,7 +48,105 @@ const sendMessage = asyncHandler( async (req, res) => {
     )
 })
 
+const getMessages = asyncHandler ( async (req, res) => {
+    const {userId} = req.params;
+    if(!isValidObjectId(userId)){
+        throw new ApiError(401, "Invalid user Object Id")
+    }
+    const messages = await Message.aggregate([
+        {
+            $match:{
+                $or:[
+                    {
+                        sender: new  mongoose.Types.ObjectId(req.user?._id),
+                        reciver: new  mongoose.Types.ObjectId(userId)
+                    },
+                    {
+                        sender: new  mongoose.Types.ObjectId(userId),
+                        reciver: new mongoose.Types.ObjectId(req.user?._id)
+                    }
+                ]
+            }
+           
+        },
+        {
+             $project:{
+                text: 1,
+                media: 1,
+                sender: 1,
+                reciver: 1
+            }
+        }
+    ])
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                messages
+            },
+            "Messages fetched Successfully"
+        )
+    )
+
+    
+})
+
+const getMessagedTo = asyncHandler( async (req, res) => {
+    const user = req.user;
+    if(!user){
+        throw new ApiError(401, "invalid User")
+    }
+    const profiles = await Message.aggregate([
+        {
+            $match:{
+                sender: new mongoose.Types.ObjectId(user._id)
+            },
+           
+        },
+        {
+            $group: {
+                _id: "$reciver"
+            }
+        },
+        {
+             $lookup:{
+                from: "users",
+                localField: "_id",
+                foreignField: "_id",
+                as: "profile"
+            }
+        },
+        {
+            $unwind: "$profile"
+        },
+        {
+            $project:{
+                "profile.avatar": 1,
+                "profile.username": 1,
+                "profile._id": 1
+            }
+        }
+    ])
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                profiles
+            },
+            "chats fetched successfully"
+        )
+    )
+
+})
+
 export {
-    sendMessage
+    sendMessage,
+    getMessages,
+    getMessagedTo
 }
 
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessagedTo, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = Router();
 
@@ -9,5 +9,7 @@ router.route("/m/:reciverId").post(verifyJwt,
     upload.array("media"),
     sendMessage
 )
+router.route("/g/:userId").get(verifyJwt, getMessages)
+router.route("/profile/chats").get(verifyJwt, getMessagedTo)
 
 export default router
